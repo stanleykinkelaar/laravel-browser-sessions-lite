7a647cef6d7a6f42381e03fbdd876f6abd5a53bb
stanleykinkelaar <14231377+stanleykinkelaar@users.noreply.github.com>
2025-11-21 15:21:20 +0000

    Fix styling

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use StanleyKinkelaar\LaravelBrowserSessionsLite;
-
 arch('it will not use debugging functions')
     ->expect('StanleyKinkelaar\LaravelBrowserSessionsLite')
     ->not->toUse(['dd', 'dump', 'ray']);
